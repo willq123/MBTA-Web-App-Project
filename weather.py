d6a4177c3dcc7210cfb1f3e03a5d5d102f2d4a57
willq123 <wqu1@babson.edu>
2024-11-11 16:18:03 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,7 +1,9 @@
-import requests
 import os
 from dotenv import load_dotenv
 from mbta_helper import get_lat_lng
+import urllib.request
+import json
+
 
 # Load environment variables from .env file
 load_dotenv()
@@ -16,11 +18,12 @@ def get_weather(location):
         raise Exception("Could not retrieve coordinates.")
     
     url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={OPENWEAHTER_API_KEY}&units=metric"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
+    
+    with urllib.request.urlopen(url) as response:
+        response_text = response.read().decode("utf-8")
+        data = json.loads(response_text)        
         weather_description = data['weather'][0]['description']
         temperature = data['main']['temp']
         return weather_description, temperature
-    else:
-        raise Exception("Could not retrieve weather data.")
+    
+    
